ec1eec0ed9fb8d05b64946261d17e7cdfe5e2020
sebastien <sebastien.caumes@hotmail.fr>
2017-02-27 23:11:17 +0100

    DI

--- NeoClientRouteur.php
+++ NeoClientRouteur.php
@@ -42,21 +42,17 @@ class NeoClientRouteur
      */
     public final function loadConf()
     {
-        if (($handle = fopen(PATH_CONF . "neocms.php", "r")) !== false) {
+        if (file_exists(PATH_CONF . "neocms.json") !== false) {
             $i       = 0;
             $clients = array();
-            while (($data = fgetcsv($handle, 1000, ";")) !== false) {
-                if ($i == 0) {
-                    $champs = $data;
-                } else {
-                    foreach ($champs as $key => $champ) {
-                        $tmp[$champ] = $data[$key];
-                    }
-                    $clients[] = $tmp;
-                }
-                $i++;
+
+            $_clients = json_decode(file_get_contents(PATH_CONF . "neocms.json"));
+
+            foreach ($_clients as $id => $cli) {
+                $cli->client_id = $id;
+                $clients[$cli->client_url] = (array) $cli;
             }
-            fclose($handle);
+
             $this->_setCache($clients);
             return $clients;
         }
@@ -68,10 +64,8 @@ class NeoClientRouteur
             $this->clients = $this->loadConf();
         }
 
-        foreach ($this->clients as $client) {
-            if ($client['client_url'] == $client_url) {
-                return $client;
-            }
+        if (isset($this->clients[$client_url])) {
+            return (array) $this->clients[$client_url];
         }
 
         return false;
@@ -80,17 +74,12 @@ class NeoClientRouteur
     private final function _setCache($clients)
     {
         //@todo linux
-        if (extension_loaded('wincache')) {
-            wincache_ucache_set('clients', $clients);
-        }
+        return false;
     }
 
     private final function _getCache($key)
     {
         //@todo linux
-        if (extension_loaded('wincache')) {
-            return wincache_ucache_get($key);
-        }
         return false;
     }
 }
