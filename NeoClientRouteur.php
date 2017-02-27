5eef05585e80ecc91034c6aab1fdfb309f46e001
sebastien <sebastien.caumes@hotmail.fr>
2017-02-27 23:17:06 +0100

    WIP

--- NeoClientRouteur.php
+++ NeoClientRouteur.php
@@ -42,6 +42,46 @@ class NeoClientRouteur
      */
     public final function loadConf()
     {
+        if (($handle = fopen(PATH_CONF . "neocms.php", "r")) !== false) {
+            $i       = 0;
+            $clients = array();
+            while (($data = fgetcsv($handle, 1000, ";")) !== false) {
+                if ($i == 0) {
+                    $champs = $data;
+                } else {
+                    foreach ($champs as $key => $champ) {
+                        $tmp[$champ] = $data[$key];
+                    }
+                    $clients[] = $tmp;
+                }
+                $i++;
+            }
+            fclose($handle);
+            $this->_setCache($clients);
+            return $clients;
+        }
+    }
+
+    private final function _checkConfUrl($client_url)
+    {
+        if ($this->clients == null && !$this->clients = $this->_getCache('clients')) {
+            $this->clients = $this->loadConf();
+        }
+
+        foreach ($this->clients as $client) {
+            if ($client['client_url'] == $client_url) {
+                return $client;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * charge la conf depuis le fichier de conf neocms.php
+     */
+    public final function loadConfJson()
+    {
         if (file_exists(PATH_CONF . "neocms.json") !== false) {
             $i       = 0;
             $clients = array();
@@ -58,7 +98,7 @@ class NeoClientRouteur
         }
     }
 
-    private final function _checkConfUrl($client_url)
+    private final function _checkConfUrlJson($client_url)
     {
         if ($this->clients == null && !$this->clients = $this->_getCache('clients')) {
             $this->clients = $this->loadConf();
